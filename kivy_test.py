03e7f5e321c535597e771fd06c12636acae15b9d
Aaron <avmagnaye@up.edu.ph>
2020-06-04 17:16:18 +0800

    added test scripts

--- kivy_test.py
+++ kivy_test.py
@@ -1,5 +1,3 @@
-# kivy_test.py
-
 import kivy
 
 from kivy.app import App
@@ -7,19 +5,30 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 
+from screens.test_screen import TestScreen
+
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 class TestApp(App):
 
     def build(self):
 
-        layout = GridLayout(cols=2)
+        # layout = GridLayout(cols=2)
 
-        layout.add_widget(Label(text='Hello 1'))
-        layout.add_widget(Label(text='World 1'))
-        layout.add_widget(Label(text='Hello 2'))
-        layout.add_widget(Label(text='World 2'))
+        # layout.add_widget(Label(text='Hello 1'))
+        # layout.add_widget(Label(text='World 1'))
+        # layout.add_widget(Label(text='Hello 2'))
+        # layout.add_widget(Label(text='World 2'))
 
-        return layout
+        sm = ScreenManager()
+
+        screens = {}
+
+        screens['test'] = TestScreen(name='test')
+
+        sm.switch_to(screens['test'])
+
+        return sm
 
 if __name__ == '__main__':
     TestApp().run()
